2bb261d04b78fef0126cf796d67cabc572b385ef
santopotter <gustavo.geldes@alumnos.uv.cl>
2019-10-07 22:32:07 -0300

    Modal, grid y backround

---
+++ src/herogrid.js
@@ -0,0 +1,52 @@
+import React, {useEffect, useState} from "react";
+import Hero  from "./components/hero"
+
+
+function HeroGrid() {
+
+  var CryptoJS = require("crypto-js");
+  var PER_KEY = "1bfa664b1ea019a1ff2389651f0e1b08";
+  var PRIV_KEY = "4fee004b323c73ee3f7f7e01715103772d9d0710";
+
+  const [heroes, setHeroes] = useState([]);
+
+  useEffect(
+    () => {
+      getHeroes()
+    }, []
+  );
+
+
+  const getHeroes = async () => {
+
+    var ts = new Date().getTime();
+    var hash = CryptoJS.MD5(ts + PRIV_KEY + PER_KEY).toString();
+    var url = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${PER_KEY}&hash=${hash}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+    setHeroes(data.data.results);
+    console.log(data.data.results);
+  }
+
+    return(
+      <div >
+        {heroes.map(hero =>
+            (
+                  <Hero
+                  key={hero.id}
+                  id={hero.id}
+                  name={hero.name}
+                  thumbnail={`${hero.thumbnail.path}/landscape_small.${hero.thumbnail.extension}`}
+                  description={hero.description}
+                  />
+
+            )
+          )}
+      </div>
+        
+        
+    )
+}
+
+export default HeroGrid;
